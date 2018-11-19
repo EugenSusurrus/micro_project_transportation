42a49ab0e26668c9b4f8a784003e72642fe76ea5
Eugen Rusu <39225487+EugenSusurrus@users.noreply.github.com>
2018-11-19 17:32:28 +0100

    Add files via upload

---
+++ test_func.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 19 16:40:38 2018
+
+@author: eugenr
+"""
+
+def test_func(Input):
+    """Unit Test Fuction"""
+    if int(Input) == 1:
+        return 1
+    if int(Input) == 2:
+        return 2
+    if int(Input) == 3:
+        return 3
+    if int(Input) == 4:
+        return 4
+    if int(Input) == 5:
+        return 5
+    if int(Input) == 6:
+        return 6
+    if int(Input) == 7:
+        return 7
+    if int(Input) == 8:
+        return 8
+    if int(Input) == 9:
+        return 9
+    if int(Input) == 10:
+        return 10
+    return -1
+
